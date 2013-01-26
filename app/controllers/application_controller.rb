1918af007f224b8ddc5d15fcc85410b57360ff5d
KoganezawaRyouta <ryouta_koganezawa@fexd.co.jp>
2013-01-26 15:36:52 +0900

    OCæʁAõæʏúAR~bg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
 class ApplicationController < ActionController::Base
+  after_filter :convert_to_utf8
   protect_from_forgery
+  
+  private
+  def convert_to_utf8
+    response.charset = "UTF-8"
+    response.body = response.body.encode("UTF-8")
+  end
 end
