202fb96b4aa599be36f8da213be173194b0a8da7
KoganezawaRyouta <ryouta_koganezawa@fexd.co.jp>
2013-06-16 15:42:02 +0900

    Ajax«ˑ¶ bèΉ

--- app/assets/javascripts/gcal_search.js
+++ app/assets/javascripts/gcal_search.js
@@ -23,7 +23,7 @@ function setEventHandler() {
 		// jQuery_Ajax_通信実行
 	    $.ajax({
 		    type: 'post',
-		    url: '/taskManagement/ajaxSetSession',
+		    url: 'training/taskManagement/ajaxSetSession',
 		    data: data,
 		    dataType: 'json',
 		    success: modalOpen
