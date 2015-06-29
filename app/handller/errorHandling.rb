796652052d4ae51fae88ee1ed8f0bf4222870498
koganezawa <koganezawa@enigmo.co.jp>
2015-06-29 21:27:35 +0900

    test2003

--- app/handller/errorHandling.rb
+++ app/handller/errorHandling.rb
@@ -1,6 +1,6 @@
 module ErrorHandling
   
-  # 500エラー
+  # 500エラー test01
   def render_500(exception = nil)
     if exception
       logger.error "Rendering 500 with exception: #{exception.message}"
@@ -8,7 +8,7 @@ module ErrorHandling
     render :file => "#{Rails.root}/public/500.html", :status => 500, :layout => false, :content_type => 'text/html'
   end
 
-  # 404エラー
+  # 404エラー test02
   def render_404(exception = nil)
     if exception
       logger.error "Rendering 404 with exception: #{exception.message}"
