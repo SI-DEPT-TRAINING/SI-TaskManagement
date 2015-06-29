0990004d0fea32ae6fcadcb7af5b69cc1a84e353
koganezawa <koganezawa@enigmo.co.jp>
2015-06-29 21:26:13 +0900

    test2001

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,10 +2,12 @@
 class LoginController < ApplicationController
   after_filter :convert_to_utf8
 
+  # 002
   def index
     reset_session
   end
 
+  # 003
   def googleOAuth
     redirect_to :controller => 'gcalSearch', :action => 'index'
   end
