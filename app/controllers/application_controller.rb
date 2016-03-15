eae82a1da23f87e8545f63df1d90659b68fad507
KoganezawaRyouta <ryouta.koganezawa@gmail.com>
2016-03-15 15:05:10 +0900

    test-01

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,7 @@ class ApplicationController < ActionController::Base
   def convert_to_utf8
     response.charset = "UTF-8"
     # test-commnet-01
+    # test-commnet-01
     response.body = response.body.encode("UTF-8")
   end
 end
